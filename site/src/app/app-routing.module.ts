48148ed94aa3ddd59c52a78ec32e4d1ddcdb733e
Mihai-Simedrea <mihai.nicolae.simedrea@gmail.com>
2024-12-30 20:50:37 +0200

    feat: remove home route

--- site/src/app/app-routing.module.ts
+++ site/src/app/app-routing.module.ts
@@ -6,8 +6,7 @@ import { TechnologyComponent } from './pages/technology/technology.component';
 import { AboutUsComponent } from './pages/about-us/about-us.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path: 'home', component: HomeComponent },
+  { path: '', component: HomeComponent, pathMatch: 'full' },
   { path: 'vision', component: VisionComponent },
   { path: 'technology', component: TechnologyComponent },
   { path: 'about-us', component: AboutUsComponent },
