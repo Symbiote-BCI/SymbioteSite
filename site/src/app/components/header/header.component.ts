876408e82e6c0bce79b79d36a908bde49233cd7f
Aleksandruu <mischie.andrey@gmail.com>
2025-01-31 21:54:35 +0200

    added 3d model

--- site/src/app/components/header/header.component.ts
+++ site/src/app/components/header/header.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
+  navExtended = false;
 
+  toggleNav() {
+    this.navExtended = !this.navExtended;
+  }
 }
--- site/src/app/components/rotating-cube/rotating-cube.component.ts
+++ site/src/app/components/rotating-cube/rotating-cube.component.ts
@@ -1,19 +1,20 @@
 import { Component, ElementRef, OnInit, OnDestroy } from '@angular/core';
 import * as THREE from 'three';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'; // Importă loaderul pentru GLB
 
 @Component({
   selector: 'app-rotating-cube',
   templateUrl: './rotating-cube.component.html',
-  styleUrls: ['./rotating-cube.component.scss']
+  styleUrls: ['./rotating-cube.component.scss'],
 })
 export class RotatingCubeComponent implements OnInit, OnDestroy {
   private scene!: THREE.Scene;
   private camera!: THREE.PerspectiveCamera;
   private renderer!: THREE.WebGLRenderer;
-  private cube!: THREE.Mesh;
+  private model!: THREE.Object3D; // Înlocuiește tipul cubului cu un tip general de obiect
   private animationFrameId: number = 0;
 
-  constructor(private elementRef: ElementRef) { }
+  constructor(private elementRef: ElementRef) {}
 
   ngOnInit(): void {
     this.init3D();
@@ -25,45 +26,80 @@ export class RotatingCubeComponent implements OnInit, OnDestroy {
       cancelAnimationFrame(this.animationFrameId);
     }
 
-    // Ensure the renderer is defined before calling dispose
     if (this.renderer) {
       this.renderer.dispose();
     }
   }
 
   private init3D() {
-    if (typeof window !== 'undefined') {  // Check if we are in the browser
-      // Create the scene
+    if (typeof window !== 'undefined') {
       this.scene = new THREE.Scene();
+      this.camera = new THREE.PerspectiveCamera(
+        75,
+        window.innerWidth / window.innerHeight,
+        0.1,
+        1000
+      );
+      this.camera.position.z = 50; // Move the camera much further away
 
-      // Set up the camera (field of view, aspect ratio, near and far clipping planes)
-      this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-      this.camera.position.z = 3;
-
-      // Set up the WebGL renderer
-      this.renderer = new THREE.WebGLRenderer();
-      this.renderer.setSize(window.innerWidth / 2, window.innerHeight / 2);
+      this.renderer = new THREE.WebGLRenderer({ antialias: true });
+      this.renderer.setSize(window.innerWidth / 1.5, window.innerHeight / 2);
       this.renderer.setClearColor(0x000000, 0);
+      this.renderer.shadowMap.enabled = true; // Enable shadow maps
       this.elementRef.nativeElement.appendChild(this.renderer.domElement);
+      this.camera.lookAt(new THREE.Vector3(0, 0, 0));
 
-      // Create a cube
-      const geometry = new THREE.BoxGeometry(1, 1, 1);
-      const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-      this.cube = new THREE.Mesh(geometry, material);
-      this.scene.add(this.cube);
+      // Add a directional light source
+      const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
+      directionalLight.position.set(10, 10, 10);
+      directionalLight.castShadow = true; // Enable shadows for the light
+      this.scene.add(directionalLight);
+
+      // Add ambient light
+      const ambientLight = new THREE.AmbientLight(0x404040); // Soft white light
+      this.scene.add(ambientLight);
+
+      const loader = new GLTFLoader();
+      loader.load('../../../assets/models/Ultracortex.glb', (gltf) => {
+        this.model = gltf.scene;
+        this.model.traverse((node) => {
+          if ((node as THREE.Mesh).isMesh) {
+            node.castShadow = true; // Enable shadows for the model
+            node.receiveShadow = true; // Enable shadows for the model
+            (node as THREE.Mesh).material = new THREE.MeshStandardMaterial({
+              color: 0x111111,
+              roughness: 0.5,
+              metalness: 0.5,
+            }); // Set the material color to black with some roughness and metalness
+          }
+        });
+
+        if (window.innerWidth < 850) {
+          this.model.scale.set(0.13, 0.13, 0.13); // Scale up the model for smaller screens
+        } else {
+          this.model.scale.set(0.18, 0.18, 0.18); // Scale up the model a little
+        }
+
+        this.scene.add(this.model);
+      });
     }
   }
 
-
   private animate() {
-    if (typeof window !== 'undefined' && typeof window.requestAnimationFrame === 'function') {
-      this.animationFrameId = window.requestAnimationFrame(() => this.animate());
+    if (
+      typeof window !== 'undefined' &&
+      typeof window.requestAnimationFrame === 'function'
+    ) {
+      this.animationFrameId = window.requestAnimationFrame(() =>
+        this.animate()
+      );
 
-      this.cube.rotation.x += 0.001;
-      this.cube.rotation.y += 0.01;
+      if (this.model) {
+        this.model.rotation.x += 0.001;
+        this.model.rotation.y += 0.01;
+      }
 
       this.renderer.render(this.scene, this.camera);
     }
   }
-
 }
